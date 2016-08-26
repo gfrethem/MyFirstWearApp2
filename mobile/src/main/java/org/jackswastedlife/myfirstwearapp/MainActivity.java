fea45c208b31ec9610ce8c362b43d9a6422638dc
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2016-08-26 00:47:07 -0500

    Mobile section of tutorial complete

--- mobile/src/main/java/org/jackswastedlife/myfirstwearapp/MainActivity.java
+++ mobile/src/main/java/org/jackswastedlife/myfirstwearapp/MainActivity.java
@@ -1,13 +1,35 @@
 package org.jackswastedlife.myfirstwearapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void sendNotification(View view) {
+        String toSend = editText.getText().toString();
+        if(toSend.isEmpty())
+            toSend = "You sent an empty notification";
+        Notification notification = new NotificationCompat.Builder(getApplication())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Android Authority")
+                .setContentText(toSend)
+                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+        int notificationId = 1;
+        notificationManager.notify(notificationId, notification);
     }
 }
